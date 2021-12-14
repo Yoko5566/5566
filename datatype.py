a8bd19cd8e94c88b50a2587202674f5b7cbe4fd8
DESKTOP-T3AB207\YOKO <ryone520@gmail.com>
2021-12-14 15:58:23 +0800

    python practice

--- datatype.py
+++ datatype.py
@@ -1,3 +1,32 @@
 # 資料 :程式的基本單位
-
-#變數 
+#數字
+3456
+3.5
+#字串
+"測試中文"
+"Hello Word"
+#布林值
+True 
+False
+# 有順序可動列表 叫做 List
+[3,4,5] #中括號 用逗號隔開不同的資料
+["Hello","World"]
+# 有順序,不可動的列表: Tuple
+[3,4,5]
+["Hello","World"]
+#集合 Set 大括號{}
+{3,4,5} #沒有大小順序差異
+{"Hello","World"}
+# 字典 Dictionary 用冒號 做配對 逗號隔開
+{"apple":"蘋果","data":"資料"}
+#變數 , 用來儲存資料的名稱
+#變數名稱 = 資料 
+#print(資料)
+x=3
+print(x)
+x=True
+print(x)
+x={3,5,6} #集合 set
+print(x)
+x="Hello"
+print(x)
